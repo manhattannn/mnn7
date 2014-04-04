20dd3e55e1c5a05c1d05f14b2327a3ede9c9d844
Alban Bailly <alban@zivtech.com>
2014-04-04 17:23:17 +0000

    ticket #104 cm theme 2 cols panel layout css inclusion and basic theming

--- sites/all/themes/cm_theme_zen/panels-layouts/cm-theme-two-columns/cm-theme-two-columns.tpl.php
+++ sites/all/themes/cm_theme_zen/panels-layouts/cm-theme-two-columns/cm-theme-two-columns.tpl.php
@@ -12,37 +12,62 @@
  * - $main_classes: Additional classes for the main region.
  */
 
+/* top row*/
 if ($top_classes) {
   print '<div class="' . $top_classes . ' row">';
 }
 else {
   print '<div class="row">';
 }
-  print $content['top'];
-  print '</div>';
+    print '<div class="inside">';
+      print $content['top'];
+    print '</div>'; /* end inside */
+  print '</div>'; /* end row */
+
+/* center row*/
+if ($center_classes) {
+  print '<div class="' . $center_classes . ' row">';
+}
+else {
   print '<div class="row">';
+}
+  print '<div class="inside">';
+
+    /* left column */
     if ($left_classes) {
       print '<div class="' . $left_classes . ' column">';
     }
     else {
       print '<div class="column">';
     }
-    print $content['left'];
-    print '</div>';
+        print '<div class="inside">';
+          print $content['left'];
+        print '</div>';
+      print '</div>';
+
+    /* right column */
     if ($right_classes) {
-    print '<div class="' . $right_classes . ' column">';
+      print '<div class="' . $right_classes . ' column">';
     }
     else {
       print '<div class="column">';
     }
-    print $content['right'];
-    print '</div>';
-  print '</div>';
-  if ($bottom_classes) {
-    print '<div class="' . $bottom_classes . ' row">';
-  }
-  else {
-    print '<div class="row">';
-  }
-  print $content['bottom'];
-  print '</div>';
+      print '<div class="inside">';
+        print $content['right'];
+      print '</div>';
+    print '</div>'; /* end column */
+
+  print '</div>'; /* end inside */
+print '</div>'; /* end row */
+
+/* bottom row*/
+if ($bottom_classes) {
+  print '<div class="' . $bottom_classes . ' row">';
+}
+else {
+  print '<div class="row">';
+}
+    print '<div class="inside">';
+     print $content['bottom'];
+    print '</div>'; /* end inside */
+  print '</div>'; /* end row */
