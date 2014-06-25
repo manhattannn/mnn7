dabad2f2cefa921a5435897ed6602187bac4cbe2
Sean Wolfe <sean@zivtech.com>
2014-06-25 20:25:14 +0000

    #151 move logo to nav region

--- sites/all/themes/mnn_theme/templates/page.tpl.php
+++ sites/all/themes/mnn_theme/templates/page.tpl.php
@@ -2,10 +2,6 @@
   <div class="wrapper header">
     <header id="header" role="banner">
 
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-      <?php endif; ?>
-
       <?php if ($site_name || $site_slogan): ?>
         <hgroup id="name-and-slogan">
           <?php if ($site_name): ?>
@@ -29,7 +25,12 @@
     </header>
   </div>
   <div class="wrapper navigation">
+
     <div id="navigation" tabindex="-1">
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+      <?php endif; ?>
+
       <?php print render($page['navigation']); ?>
     </div><!-- /#navigation -->
   </div>
