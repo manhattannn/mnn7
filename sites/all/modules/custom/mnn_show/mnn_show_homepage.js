1e542c8efea1230833589e80662beaa4fb30cdde
Alban Bailly <alban@zivtech.com>
2014-07-01 22:59:58 +0000

    ticket #170 work on watch now channels sidebar

--- sites/all/modules/custom/mnn_show/mnn_show_homepage.js
+++ sites/all/modules/custom/mnn_show/mnn_show_homepage.js
@@ -130,9 +130,11 @@ Object.size = function(obj) {
     if ($('#block-mnn-show-watch-now').length){
       scheduleNowPlaying.init();
     }
-    $('#block-mnn-show-watch-now .channel-info h5 a').click(function(e) {
+    $('#block-mnn-show-watch-now .channel-info .channel-about').addClass('segment-hidden');
+    $('#block-mnn-show-watch-now .channel-info h4').click(function(e) {
       e.preventDefault();
-      $(this).parent('h5').parent('.channel-info').children('.channel-about').toggleClass('segment-hidden');
+      $(this).toggleClass('down');
+      $(this).parent('.channel-info').children('.channel-about').toggleClass('segment-hidden');
     });
   });
 }(jQuery));
